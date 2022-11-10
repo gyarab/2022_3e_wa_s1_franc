c6223ad9baa033ad3de3800d6412781484c7e033
Fravoj3 <vojtech.franc@student.gyarab.cz>
2022-11-10 18:25:08 +0100

    Přidán úkol se šachovnicí

---
+++ MinimalniSachovnice/index.php
@@ -0,0 +1,1 @@
+<!DOCTYPE html><title>♖</title><table><?php $t='<tr style="height:82px;">';$u='<td style="width:80px; "></td>';echo($t);for($i=0;$i<64;$i++){echo((strval($i/8%2)-$i%2?str_replace(' ','background-color:black;',$u):$u).(($i+1)%8==0?'</tr>'.($i!=63?$t:''):''));}?></table>
